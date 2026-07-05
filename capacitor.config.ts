5041eefbd3567924fe1d0db4b2055dad9b907bf8
yct1998 <yct200113@gmail.com>
2026-07-05 21:18:44 +0800

    Add Capacitor Android config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "io.github.godrenech.cddaguide",
+  appName: "CDDA Guide",
+  webDir: "dist",
+};
+
+export default config;
